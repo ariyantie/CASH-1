56b10301a2f5bd177eaa57abe63d5cd48e72348b
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-10-10 18:39:59 +0300

    Ask for confirmation before logging out from the Make Payment screen

--- app/src/main/java/com/android/cash1/activities/MakePaymentActivity.java
+++ app/src/main/java/com/android/cash1/activities/MakePaymentActivity.java
@@ -1,15 +1,21 @@
 package com.android.cash1.activities;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.cash1.R;
+import com.android.cash1.activities.login.LogoutActivity;
 import com.android.cash1.model.Cash1Activity;
 import com.android.cash1.rest.Cash1ApiService;
 import com.android.cash1.rest.Cash1Client;
@@ -135,5 +141,52 @@ public class MakePaymentActivity extends Cash1Activity {
             }
         });
         */
+    }
+
+    @Override
+    public void logout(View view) {
+        showConfirmLogoutPopup();
+    }
+
+    private void showConfirmLogoutPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View rootView = inflater.inflate(R.layout.dialog, null);
+        builder.setView(rootView);
+
+        rootView.findViewById(R.id.spinner).setVisibility(View.GONE);
+        TextView titleView = (TextView) rootView.findViewById(R.id.title);
+        titleView.setVisibility(View.VISIBLE);
+        titleView.setText("Payment Has Not Made");
+        TextView messageView = (TextView) rootView.findViewById(R.id.body);
+        messageView.setVisibility(View.VISIBLE);
+        messageView.setText("Are you sure you want to log out? Your current payment information will not be saved.");
+
+        final AlertDialog dialog = builder.create();
+
+        Button confirmButton = (Button) rootView.findViewById(R.id.confirm);
+        confirmButton.setVisibility(View.VISIBLE);
+        confirmButton.setText("Log Out");
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MakePaymentActivity.this, LogoutActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancel);
+        cancelButton.setVisibility(View.VISIBLE);
+        cancelButton.setText("Return to Payment");
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
     }
 }
